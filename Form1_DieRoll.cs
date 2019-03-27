73fc9db94790e4e45bd521600b613cd29710432f
BKernohan36 <47619311+BKernohan36@users.noreply.github.com>
2019-03-26 21:18:53 -0600

    Update and rename Form1.cs to Form1_DieRoll.cs

--- Form1_DieRoll.cs
+++ Form1_DieRoll.cs
@@ -1,4 +1,21 @@
-﻿using System;
+/**
+ * @author Brent Kernohan
+ * 
+ * @description Simulates die rolls with the Random class which is based on statistical requirements.
+ * Once the die object is created with the Random class, a interval from 1 to selected die sides.
+ * i.e. if the d100 die is selected for the simulation, the interval used would be from 1 - 100.
+ * Furthermore, the random generated roll can be added with a constant defined by the players or the DM.
+ * 
+ * @todo
+ *  -add in the operations: multiplication, subtraction, and division to add more benefit of using the app 
+ *  -add the ability to do multiple die rolls in the form
+ *
+ * @bugs
+ *  -fix the instance when button.dNum is pressed and clear after the re-selection of the button
+ *
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
