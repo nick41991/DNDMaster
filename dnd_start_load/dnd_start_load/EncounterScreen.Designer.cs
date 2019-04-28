aa52479654a624b8a8dea5f2a848987c18cb8429
Daniel Aranda <danielote97@gmail.com>
2019-04-28 01:21:39 -0600

    newdesign

--- dnd_start_load/dnd_start_load/EncounterScreen.Designer.cs
+++ dnd_start_load/dnd_start_load/EncounterScreen.Designer.cs
@@ -28,28 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.playernames = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.OrangeRed;
-            this.panel1.Controls.Add(this.listView1);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(156, 734);
-            this.panel1.TabIndex = 0;
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(0, 401);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(155, 27);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // panel2
             // 
@@ -59,24 +40,32 @@
             this.panel2.Size = new System.Drawing.Size(155, 690);
             this.panel2.TabIndex = 1;
             // 
+            // playernames
+            // 
+            this.playernames.BackColor = System.Drawing.Color.OrangeRed;
+            this.playernames.Location = new System.Drawing.Point(0, 0);
+            this.playernames.Multiline = true;
+            this.playernames.Name = "playernames";
+            this.playernames.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.playernames.Size = new System.Drawing.Size(172, 690);
+            this.playernames.TabIndex = 0;
+            // 
             // EncounterScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.playernames);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.Name = "EncounterScreen";
             this.Size = new System.Drawing.Size(1024, 690);
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox playernames;
     }
 }
