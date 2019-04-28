66090e56d149171a43bbf2444fd7e2ef86c9a2c6
Nick <nick41991.s@gmail.com>
2019-04-27 20:12:49 -0600

    Comments

--- dnd_start_load/dnd_start_load/Form2.cs
+++ dnd_start_load/dnd_start_load/Form2.cs
@@ -71,7 +71,7 @@ namespace dnd_start_load
             signal = 6;
             //Need to check if die roller window is open and if not open new one, else bring to focus
             /* Possibly will work
-            if (Application.OpenForms.OfType<Form1_DieRoll>.Count() == 1)
+            if (Application.OpenForms.OfType<Form1_DieRoll>.Count() = 1)
             {
                 foreach (Form1_DieRoll roller in Application.OpenForms.OfType<Form1_DieRoll>) {
                     roller.WindowState = FormWindowState.Minimized;
