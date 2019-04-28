fbb1db0ec082727f60bf545f80b12f9ea83f8426
Daniel Aranda <danielote97@gmail.com>
2019-04-28 12:18:56 -0600

    monster stats added

--- dnd_start_load/dnd_start_load/EncounterScreen.Designer.cs
+++ dnd_start_load/dnd_start_load/EncounterScreen.Designer.cs
@@ -32,29 +32,29 @@
             this.startbutton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.updatebutton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // playernames
             // 
             this.playernames.BackColor = System.Drawing.SystemColors.ControlLight;
             this.playernames.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.playernames.Location = new System.Drawing.Point(-3, 82);
-            this.playernames.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.playernames.Location = new System.Drawing.Point(-2, 67);
             this.playernames.Multiline = true;
             this.playernames.Name = "playernames";
             this.playernames.ReadOnly = true;
             this.playernames.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.playernames.Size = new System.Drawing.Size(371, 680);
+            this.playernames.Size = new System.Drawing.Size(279, 205);
             this.playernames.TabIndex = 0;
             this.playernames.TextChanged += new System.EventHandler(this.playernames_TextChanged);
             // 
             // startbutton
             // 
             this.startbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.startbutton.Location = new System.Drawing.Point(776, 265);
-            this.startbutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startbutton.Location = new System.Drawing.Point(582, 215);
             this.startbutton.Name = "startbutton";
-            this.startbutton.Size = new System.Drawing.Size(216, 57);
+            this.startbutton.Size = new System.Drawing.Size(162, 46);
             this.startbutton.TabIndex = 2;
             this.startbutton.Text = "Start";
             this.startbutton.UseVisualStyleBackColor = true;
@@ -65,36 +65,58 @@
             this.label1.BackColor = System.Drawing.Color.OrangeRed;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(-1, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(371, 84);
+            this.label1.Size = new System.Drawing.Size(278, 68);
             this.label1.TabIndex = 3;
             this.label1.Text = "Players";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // updatebutton
             // 
-            this.updatebutton.Location = new System.Drawing.Point(1213, 0);
-            this.updatebutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.updatebutton.Location = new System.Drawing.Point(910, 0);
             this.updatebutton.Name = "updatebutton";
-            this.updatebutton.Size = new System.Drawing.Size(129, 54);
+            this.updatebutton.Size = new System.Drawing.Size(97, 44);
             this.updatebutton.TabIndex = 4;
             this.updatebutton.Text = "Update Players";
             this.updatebutton.UseVisualStyleBackColor = true;
             this.updatebutton.Click += new System.EventHandler(this.updatebutton_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(-2, 338);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(279, 282);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.OrangeRed;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-1, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 68);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Monster Stats";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // EncounterScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.updatebutton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.startbutton);
             this.Controls.Add(this.playernames);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "EncounterScreen";
-            this.Size = new System.Drawing.Size(1343, 763);
+            this.Size = new System.Drawing.Size(1007, 620);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,5 +127,7 @@
         private System.Windows.Forms.Button startbutton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button updatebutton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
